0796720764f3bc6f0baa778690ad9daa91369e0b
Kostyasan1214 <166634803+Kostyasan1214@users.noreply.github.com>
2024-04-10 18:25:27 +0300

    Source added. Wow

---
+++ Source.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n=13;
+	if (n + 3 < 1488) {
+		cout << "123";
+
+	}
+	else {
+		cout << "Action rerorcefdgp";
+	}
+	
+}
